fb386c70a8d2512d7d7317a24031da106f263981
Alan Hong <xiupan@gmail.com>
2017-07-03 14:33:15 -0500

    Now works with MongoDB! Yay!

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const data = require('./data.js');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
 const router = require('./routes/index');
+const http = require('http');
+const mongo = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/robotDB";
 
 const app = express();
 
@@ -13,6 +17,22 @@ app.set('view engine', 'mustache');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('Successfully connected to the database');
+    }
+    const data = require("./data");
+    for (var i = 0; i < data.users.length; i++) {
+      const user = data.users[i];
+      db.collection("users").updateOne(
+        {id: user.id},
+        user,
+        {upsert: true}
+      )
+    }
+  })
   res.render('index', {users:data.users});
 })
 
