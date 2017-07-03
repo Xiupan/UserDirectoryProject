de7d68b199142fd0c7b1b9c9dfab0d53a3fe2f94
Alan Hong <xiupan@gmail.com>
2017-07-03 13:39:39 -0500

    Base working condition before re-factor with MongoDB.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const data = require('./data.js');
+const app = express();
+const data = require('../data.js');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-const router = require('./routes/index');
-
-const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 app.get('/', function (req, res) {
   res.render('index', {users:data.users});
@@ -19,8 +17,4 @@ app.get('/', function (req, res) {
 app.get('/:id', function(req, res){
   var inputID = req.params.id - 1;
   res.render('user', {users:data.users[inputID]});
-})
-
-app.listen(3000, function () {
-  console.log('Successfully started User Directory application!');
 })
